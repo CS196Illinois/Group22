d8183df2a1b942dff7aa723211c48f6a21711aee
nithilak <nithilak@illinois.edu>
2021-10-23 18:58:06 -0500

    Create App.js
    just testing, working with adding a button

---
+++ Research/nithilak/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+
+  <div className="App">
+    <button type="button" onclick="alert('Hello world!')"> hello</button>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>
+        React App
+      </h2>
+        <p>
+          test
+        </p>
+    </header>
+  </div>
+  //</BrowserRouter>
+  );
+}
+
+export default App;
