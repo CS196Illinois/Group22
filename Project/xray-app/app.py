d4f785734026c1299c92c5f097bb05426e043b58
fshaik032 <fshaik032@gmail.com>
2021-12-08 00:22:19 -0600

    Added db file to store images from upload. Added route to acces the image stored in the db file. I also converted to the react components into a normal html file because I found it was easier to render them using flask with the render_template() function. I will convert it back to React once I figure out how to. I also deployed the website to https://xrayclassifier22.herokuapp.com/

---
+++ Project/xray-app/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, Response
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pics.db"
+db = SQLAlchemy(app)
+uploaded = False
+
+
+class Img(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Text, unique=True, nullable=False)
+    name = db.Column(db.Text, nullable=False)
+    mimetype = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return "<Task %r>" % self.id
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    # if it's a post request, we add the image to the db file
+    if request.method == "POST":
+        pic = request.files['pic']
+        if not pic:
+            return 'No pic uploaded!', 400
+
+        filename = secure_filename(pic.filename)
+        mimetype = pic.mimetype
+        if not filename or not mimetype:
+            return 'Bad upload!', 400
+
+        img = Img(img=pic.read(), name=filename, mimetype=mimetype)
+        # clear the table before adding a new image
+        num_rows_deleted = db.session.query(Img).delete()
+        db.session.commit()
+
+        db.session.add(img)
+        db.session.commit()
+        global uploaded
+        # we change the uploaded boolean to true so the html page wont't show the upload button anymore
+        uploaded = True
+        return render_template("index.html", uploaded=uploaded)
+    else:
+        return render_template("index.html")
+
+
+@app.route('/<int:id>')
+def get_img(id):
+    img = Img.query.filter_by(id=id).first()
+    if not img:
+        return 'Img Not Found!', 404
+
+    return Response(img.img, mimetype=img.mimetype)
+
+# route for deleting image
+@app.route("/delete/")
+def delete():
+    try:
+        num_rows_deleted = db.session.query(Img).delete()
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- Project/xray-app/src/components/Description.js
+++
@@ -1,8 +0,0 @@
-import "./Description.css"
-function Description() {
-    return (
-        <p className='description'>With Chest X-ray Classifier, you can quickly learn whether an X-ray shows signs of Pneumonia</p>
-    );
-  }
-  export default Description;
-  
